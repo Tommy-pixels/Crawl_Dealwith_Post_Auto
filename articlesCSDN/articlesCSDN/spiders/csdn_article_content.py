7dd08e8239997201866f21af59efa329fabca6c7
Eighteen <dreamertgq@163.com>
2022-03-23 16:39:43 +0800

    20220323 CSDN优优优化

--- articlesCSDN/articlesCSDN/spiders/csdn_article_content.py
+++ articlesCSDN/articlesCSDN/spiders/csdn_article_content.py
@@ -1,5 +1,5 @@
 import scrapy, time
-from fake_useragent import UserAgent
+# from fake_useragent import UserAgent
 from .. import items
 from auto_datahandler.customFunction__.Cleaner.cleaner_paragraph import Cleaner_Paragraph
 from auto_datahandler.basement__.IsCheck import IsCheck_uchar
@@ -34,7 +34,6 @@ def _str_check(s):
 
 class CSDNSpider(scrapy.Spider):
     name = 'csdnarticleSpider'
-    start_url = 'https://blog.csdn.net/qq_19782019/article/details/80259836?ops_request_misc=&request_id=&biz_id=102&utm_term=%E6%8E%A5%E5%8F%A3&utm_medium=distribute.pc_search_result.none-task-blog-2~all~sobaiduweb~default-0-80259836.nonecase&spm=1018.2226.3001.4449'
     headers = {
         'Host': 'blog.csdn.net',
         'Connection': 'keep-alive',
@@ -58,7 +57,8 @@ class CSDNSpider(scrapy.Spider):
     UPDATE_NOTE = "UPDATE `tb_article` SET `note` = '{}' WHERE (`id` = '{}');"
 
     def start_requests(self):
-        sql_origin = 'SELECT `id`,`ori_url`,`title` FROM `dbfreeh`.`tb_article` WHERE (`id`>1000 and `id`<2000) and (`note` is null or `note`="") and (`content`="" or `content` is null);'
+        sql_origin = 'SELECT `id`,`ori_url`,`title` FROM `dbfreeh`.`tb_article` WHERE (`id`>8000 and `id`<9000) and (`note` is null or `note`="") and (`content`="" or `content` is null);'
+        # sql_origin = 'SELECT `id`,`ori_url`,`title` FROM `dbfreeh`.`tb_article` WHERE (`id`=4129);'
         # sql_ = "SELECT `id`,`ori_url`,`title` FROM `dbfreeh`.`tb_article` WHERE `content`='' AND `note` is Null and `id`>1037 and `id`<2000;"
         self.db.cursor.execute(sql_origin)
         article_lis = self.db.cursor.fetchall()
@@ -74,22 +74,46 @@ class CSDNSpider(scrapy.Spider):
             elif('ask.csdn.net' in url):
                 self.db.cursor.execute(self.UPDATE_NOTE.format('回答页面', id_a))
                 continue
-            elif ('category_' in url):
+            elif ('category_' in url or '/category' in url):
                 self.db.cursor.execute(self.UPDATE_NOTE.format('列表页面', id_a))
                 continue
-            self.headers['User-Agent'] = str(UserAgent().random)
+            elif ('bbs.csdn.net' in url):
+                self.db.cursor.execute(self.UPDATE_NOTE.format('帖子页面', id_a))
+                continue
+            # self.headers['User-Agent'] = str(UserAgent().random)
             dic = {}
             dic['id_a'] = id_a
-            yield scrapy.Request(url=url, headers=self.headers, cookies=self.cookies, callback=self.parse_content, cb_kwargs=dic)
+            if('https' not in url and 'http' in url):
+                url = url.replace('http','https')
+            yield scrapy.Request(url=url, headers=self.headers, cookies=self.cookies, callback=self.parse_content, cb_kwargs=dic,dont_filter=True)
             time.sleep(5)
 
     def parse_content(self, response, id_a):
-        par_xpath_s = '//div[@class="blog-content-box"]/article/div[@id="article_content"]/div[@id="content_views"]'
-        articleContentItem = items.ArticleContentItem()
-        pList = response.xpath(par_xpath_s + '/*')
-        cleaner_paragraph = Cleaner_Paragraph()
+        try:
+            # 对于404页面的处理
+            div_404 = response.xpath('//div[@class="new_404"]')
+            print(id_a, ' 页面无效')
+            if (div_404):
+                self.db.cursor.execute(self.UPDATE_NOTE.format('页面无效', id_a))
+                print(id_a, ' 页面无效')
+        except Exception as e:
+            print(id_a, ' 页面无效')
 
+        # 4.1 需要跳过的
+        self.lis_continue = ['本分享为', 'QQ交流群', '更多分享', '作者：', '来源：', '原文：', '版权声明：', '文章出自', '公众号：', '抖音号：', '版权声明：', '原文链接：',
+                        '重金招聘', '题来自', '点击上方', 'CSDN编者', '作者介绍', '头|', 'CSDN：', '日期：',
+                        '转自公众号', '转载自', '目录', '前言', '浏览量：', '匿名', '作者:', '编辑：', '扫码关注', '文章推荐', '关注回复', '禁止转载', '本文由',
+                        '转自：', '点个赞+', '链接：', '参考资料：', '请关注：', '关注：', '微信号：', '部分内容参考自', '[关于我们]', '说明：', '星标公众号',
+                        '请参看我', '点击关注', '作者|', '来源|', '戳进去领取', '加我微信', '关注我们', '未经允许', '原文链接', '点在看', '出品|', '编译|', '责编|',
+                        '阅读原文', '更多精彩', '参考资料', '相关的资料链接戳这里', '作者/', '|知乎', '|博客', '交流群', '转发吧', '点“在看”', '文末福利', '免费获取', '文/',
+                        '扫描上方二维码', '点击', '作者｜', '转自','校对｜', '微信群', '关注', '转载', '参见', '阅读本文', '关注', '往期精彩回顾', '原文地址：', '相关文档', '来源于',
+                        '转载于']
+        articleContentItem = items.ArticleContentItem()
+        cleaner_paragraph = Cleaner_Paragraph()
+        par_xpath_s = '//div[@class="blog-content-box"]/article/div[@id="article_content"]/div[@id="content_views"]'
+        pList = response.xpath(par_xpath_s + '/*')
         special = response.xpath(par_xpath_s + '/*/text()').extract()
+
         if (len(pList) == 2 and 'svg' in pList[0].extract()):
             special = response.xpath(par_xpath_s + '/*')[1].xpath('./*/text()').extract()
         if(((len(special) == 1 and special[0].strip() == '') or not response.xpath(par_xpath_s + '/*/text()').extract()) and response.xpath(par_xpath_s).xpath('string(.)').extract_first().replace(' ','') !=''):
@@ -98,11 +122,79 @@ class CSDNSpider(scrapy.Spider):
             articleContentItem['id_a'] = id_a
             articleContentItem['content'] = content
             yield articleContentItem
-        elif(len(special)>2 and '转载于' in ''.join(special)):
+        elif(len(special)>2 and '转载于' not in ''.join(special) and 'class="postText"' not in pList[0].extract()):
             content = ''
             for p in special:
                 c = cleaner_paragraph.integratedOp(p)
                 if(c):
+                    content = content + '<p>' + c + '</p>'
+            articleContentItem['id_a'] = id_a
+            articleContentItem['content'] = content
+            yield articleContentItem
+        elif ((len(pList) == 2 and pList[1].extract().startswith('<p>')) and '转载于' not in ''.join(pList[1].extract())):
+            pList = response.xpath(par_xpath_s + '/*/text()').extract()
+            imgList = response.xpath(par_xpath_s + '/*')[1].xpath('./*')
+            content = ''
+            for p in pList:
+                check = False
+                for i_continue in self.lis_continue:
+                    if (i_continue in p):
+                        check = True
+                        break
+                if(check):
+                    continue
+                c = cleaner_paragraph.integratedOp(p)
+                # if(int(len(c))<3):
+                #     pass
+                # else:
+                if(c):
+                    content = content + '<p>' + c + '</p>'
+            if(imgList):
+                for img in imgList:
+                    if ('<img' in img.extract()):
+                        src = img.xpath('./@src').extract_first()
+                        if(src):
+                            content = content + "<img src='http://119.23.244.126/get_img?img_url=" + img.xpath('./@src').extract_first() + "' />"
+            articleContentItem['id_a'] = id_a
+            articleContentItem['content'] = content
+            yield articleContentItem
+        elif(''.join(special).replace(' ','')=='' and response.xpath(par_xpath_s + '/text()').extract()):
+            pList = response.xpath(par_xpath_s + '/text()').extract()
+            content = ''
+            for p in pList:
+                check = False
+                for i_continue in self.lis_continue:
+                    if (i_continue in p):
+                        check = True
+                        break
+                if (check):
+                    continue
+                c = cleaner_paragraph.integratedOp(p)
+                # if(int(len(c))<3):
+                #     pass
+                # else:
+                if (c):
+                    content = content + '<p>' + c + '</p>'
+            articleContentItem['id_a'] = id_a
+            articleContentItem['content'] = content
+            yield articleContentItem
+        elif(len(pList)==1 and ''.join(pList[0].xpath('./*/text()'))==''):
+            # 【特殊情况】针对只有文本节点
+            pList = pList[0].xpath('./text()').extract()
+            content = ''
+            for p in pList:
+                check = False
+                for i_continue in self.lis_continue:
+                    if (i_continue in p):
+                        check = True
+                        break
+                if (check):
+                    continue
+                c = cleaner_paragraph.integratedOp(p)
+                # if(int(len(c))<3):
+                #     pass
+                # else:
+                if (c):
                     content = content + '<p>' + c + '</p>'
             articleContentItem['id_a'] = id_a
             articleContentItem['content'] = content
@@ -119,7 +211,7 @@ class CSDNSpider(scrapy.Spider):
                 pList = response.xpath(par_xpath_s + '/*')[2].xpath('./*')
                 if (len(pList) == 1):
                     pList = response.xpath(par_xpath_s + '/*')[2].xpath('./*')[0].xpath('./*')
-            elif(len(pList)<2):
+            elif(len(pList)<2 and not pList[0].extract().startswith('<p')):
                 pList = response.xpath(par_xpath_s + '/*')[0].xpath('./*')
                 if(len(pList)==1):
                     pList = response.xpath(par_xpath_s + '/*')[0].xpath('./*')[0].xpath('./*')
@@ -134,6 +226,10 @@ class CSDNSpider(scrapy.Spider):
                 pList = response.xpath(par_xpath_s + '/*')[1].xpath('./*')
                 if(len(pList) == 1):
                     pList = response.xpath(par_xpath_s + '/*')[1].xpath('./*')[0].xpath('./*')
+            elif(len(pList)==2 and 'class="postText"' in pList[0].extract()):
+                pList = response.xpath(par_xpath_s + '/*')[0].xpath('./*')
+                if (len(pList) == 1):
+                    pList = response.xpath(par_xpath_s + '/*')[0].xpath('./*')[0].xpath('./*')
             elif(len(pList)==2 and '转载于' in pList[1].extract()):
                 pList = response.xpath(par_xpath_s + '/*')[0].xpath('./*')
                 if (len(pList) == 1):
@@ -143,6 +239,8 @@ class CSDNSpider(scrapy.Spider):
                 pList = response.xpath(par_xpath_s + '/*')[0].xpath('./*')
                 if(len(pList) == 1):
                     pList = response.xpath(par_xpath_s + '/*')[1].xpath('./*')[0].xpath('./*')
+            elif(len(pList) == 1):
+                pList = response.xpath(par_xpath_s + '/*')[0].xpath('./*')
             content = ''
 
             # 判断文章是否vip文章或是专栏文章
@@ -189,20 +287,13 @@ class CSDNSpider(scrapy.Spider):
             for p in pList:
                 c = p.xpath('string(.)').extract_first().replace('\u3000', '').replace(' ','').replace('　', '').replace('\xa0','').replace('\r','').replace('\n','').replace('\t','')
                 # 指定内容才筛选链接
-                if((0<len(c)<80 and _str_check(c)) or 0<len(c)<=2 or (len(c)==0 and p.xpath('.//img') == []) or len(c.replace('-',''))==0):
+                if((0<len(c)<80 and _str_check(c)) or 0<len(c)<=2 or (len(c)==0 and p.xpath('.//img') == []) or (len(c.replace('-',''))==0 and p.xpath('.//img') == [])):
                     continue
-                if ('版权说明' in c or '下面二维码' in c or '可关注微信公众号' in c or '微信公众号' in c or '邮箱地址' in c or '请关注公众号' in c or '相关系列：' in c
+                if ('版权说明' in c or '下面二维码' in c or '可关注微信公众号' in c or '邮箱地址' in c or '请关注公众号' in c or '相关系列：' in c
                     or 'END' in c or '推荐阅读：' in c or '加微信' in c or '扫码下面二维码' in c or ('参考资料' in c and len(c)<6) or '往期推荐' in c or '相关链接'in c):
                     break
-                # 4.1 需要跳过的
-                lis_continue = ['本分享为', 'QQ交流群', '更多分享', '作者：', '来源：', '原文：', '版权声明：', '文章出自', '公众号：', '抖音号：', '版权声明：','原文链接：','重金招聘',
-                                '转自公众号', '转载自', '目录', '前言', '浏览量：', '匿名', '作者:', '编辑：', '扫码关注', '文章推荐', '关注回复', '禁止转载', '本文由', '题来自', '点击上方',
-                                '转自：', '点个赞+', '链接：','参考资料：', '请关注：', '关注：','微信号：', '部分内容参考自', '[关于我们]','说明：', '星标公众号','请参看我','点击关注','作者|',
-                                '来源|', '戳进去领取','加我微信', '关注我们', '未经允许','原文链接','点在看','出品|','编译|','责编|','阅读原文','更多精彩','参考资料','相关的资料链接戳这里','作者/',
-                                '|知乎','|博客','交流群','转发吧','点“在看”','文末福利', '免费获取','文/', 'CSDN编者','作者介绍','头|','CSDN：','扫描上方二维码','点击','作者｜', '转自'
-                                '校对｜','微信群','关注', '转载']
                 check = False
-                for i_continue in lis_continue:
+                for i_continue in self.lis_continue:
                     if(i_continue in c):
                         check = True
                         break
